ab29c8dfc4a1cdd1f77633e33382b8858093a92a
Y Naga Phaneendra <your-email@example.com>
2026-08-13 07:43:10 +0530

    Fixed Tailwind plugin

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { AuthProvider } from "./context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/club-portal">
       <AuthProvider>
         <App />
       </AuthProvider>
